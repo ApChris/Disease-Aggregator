32b510fe29ce962fb91b1e37065ddb5ba562d7fa
ApChris <achris.info@gmail.com>
2020-05-11 16:53:50 +0300

    update structures from list.h

---
+++ include/list.h
@@ -0,0 +1,33 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#define FLAG 1400322
+
+typedef struct PathNode
+{
+    char * path;
+    struct PathNode * next;
+}PathNode;
+
+
+typedef struct Node
+{
+    long item;
+    struct Node * next;
+}Node;
+
+
+void PushNode(Node ** head, long data);
+bool SearchInList(Node ** head, long data);
+long LenOfList(void * head);
+void DeleteList(Node ** head);
+void DeleteNode(Node ** head, long data);
+void PrintList(Node ** head);
+
+void PushNode_Path(PathNode ** head, char * path);
+bool SearchInList_Path(PathNode ** head, char * path);
+void DeleteList_Path(PathNode ** head);
+void DeleteNode_Path(PathNode ** head, char * path);
+void PrintList_Path(PathNode ** head);
+
+#endif
