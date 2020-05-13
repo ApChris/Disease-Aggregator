8338c5300a0e236dfc98a3571f22ae2bd21beef0
ApChris <achris.info@gmail.com>
2020-05-14 00:00:44 +0300

    parent Fucntion has been updated with CreateWorker, I add more functions there

--- include/parentFunctions.h
+++ include/parentFunctions.h
@@ -1,5 +1,11 @@
 #ifndef __PARENTFUNCTIONS_H_
 #define __PARENTFUNCTIONS_H_
 
+#include "../include/parentNamedPipesFifo.h"
+#include "../include/list.h"
+#define PROCESSIDSTRING 50
+
+
+long CreateWorker(long processID, long totalWorkers, PathNode * subDirectoriesPathList);
 
 #endif
