288db2e727abbe124850aa256b1fb9795b7189fb
ApChris <achris.info@gmail.com>
2020-05-13 23:59:59 +0300

    initial commit

---
+++ include/workerNamedPipesFifo.h
@@ -0,0 +1,26 @@
+#ifndef __WORKERNAMEDPIPESFIFO_H__
+#define __WORKERNAMEDPIPESFIFO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define FIFOPATH "etc/fifo/myFIFO"
+
+#define MBUFFER 1000
+#define INITCOUNTER 0
+
+
+bool UnlinkNamedPipe_FIFO(long pid, char * flag);
+long ReadFromNamedPipe(long fileDescriptor, char * buffer);
+void WriteToNamedPipe(long fileDescriptor, char * buffer);
+long OpenRead(long pid);
+long OpenWrite(long pid);
+
+#endif
