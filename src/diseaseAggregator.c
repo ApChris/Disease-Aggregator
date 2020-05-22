49df416da9a9d5f6dce2c22c1148b20f6d5775fb
ApChris <achris.info@gmail.com>
2020-05-22 20:02:54 +0300

    request 3

--- src/diseaseAggregator.c
+++ src/diseaseAggregator.c
@@ -180,7 +180,95 @@ bool Request_2(char * tok)
     printf("%ld\n",res);
 }
 
+bool Request_3(char * tok)
+{
+    char message[MAXIMUMBUFFER];
+    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
 
+    // store k
+    char * k;
+    tok = strtok(NULL," ");
+    k = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+    strcpy(k,(const  char *)tok);
+
+    // store country
+    tok = strtok(NULL," ");
+    char * country;
+    country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+    strcpy(country,(const  char *)tok);
+
+    // store diseaseID
+    char * diseaseID;
+    tok = strtok(NULL," ");
+    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+    strcpy(diseaseID,(const  char *)tok);
+
+
+    Date * date1 = NULL;
+    Date * date2 = NULL;
+    date1 = malloc(sizeof(*date1));
+    date2 = malloc(sizeof(*date2));
+
+    // // date1
+    tok = strtok(NULL, delimiters);
+    date1 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> year = (long)atoi(tok);
+
+    // date2
+    tok = strtok(NULL,delimiters);
+    date2 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> year = (long)atoi(tok);
+    //
+    //
+    printf("%s %s %s %ld-%ld-%ld %ld-%ld-%ld" ,k ,country, diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
+    for (size_t i = 0; i < totalWorkers; i++)
+    {
+
+        sprintf(message,"/topk-AgeRanges %s %s %s %ld-%ld-%ld %ld-%ld-%ld",k, country, diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
+
+        WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+        kill(GetValue(&workersPidList,i),SIGUSR1);
+    }
+    //
+    //
+    //
+    //
+    // free(date1);
+    // free(date2);
+    //
+    long arrayOfK[totalWorkers];
+    char result[512];
+    int bytes;
+    long res = 0;
+    for (long i = 0; i < totalWorkers; i++)
+    {
+        do
+        {
+            usleep(100000);
+
+        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
+        arrayOfK[i] = res;
+
+    }
+
+    for (long i = 0; i < atol(k); i++)
+    {
+        printf("%ld\n",arrayOfK[i]);
+    }
+    // printf("%ld\n",res);
+}
+
+// /topk-AgeRanges 3 China COVID-2019 10-10-2010 10-10-2020
 
 void Request_4(char * recordID)
 {
@@ -214,6 +302,279 @@ void Request_4(char * recordID)
 }
 
 
+bool Request_5(char * tok)
+{
+    char message[MAXIMUMBUFFER];
+    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
+
+    char * diseaseID;
+
+    tok = strtok(NULL," ");
+    if(tok == NULL)
+    {
+        printf("error\n");
+        return true;
+    }
+    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+    strcpy(diseaseID,(const  char *)tok);
+
+
+    Date * date1 = NULL;
+    Date * date2 = NULL;
+    date1 = malloc(sizeof(*date1));
+    date2 = malloc(sizeof(*date2));
+
+    // // date1
+    tok = strtok(NULL, delimiters);
+
+    date1 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> year = (long)atoi(tok);
+
+    // date2
+    tok = strtok(NULL,delimiters);
+
+    if(tok == NULL)
+    {
+        free(date1);
+        free(date2);
+        printf("error\n");
+        return true;
+    }
+    // date2
+    date2 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> year = (long)atoi(tok);
+
+
+    tok = strtok(NULL,delimiters);
+    // without country
+    char * country = NULL;
+
+    if(tok == NULL)
+    {
+
+        for (size_t i = 0; i < totalWorkers; i++)
+        {
+
+            sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
+
+            WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+            kill(GetValue(&workersPidList,i),SIGUSR1);
+        }
+    }
+    else
+    {
+        // store country
+
+        country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+        strcpy(country,(const  char *)tok);
+
+        for (size_t i = 0; i < totalWorkers; i++)
+        {
+
+            char * path = malloc(sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)) + 1);
+            strcpy(path,GetValue_Path(&subDirectoriesPathList,i));
+            path = strtok(tok,delimiters);
+            path = strtok(tok,delimiters);
+            path = strtok(tok,delimiters);
+
+            if(!strcmp(path,country))
+            {
+                sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld %s",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year, country);
+
+                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+                kill(GetValue(&workersPidList,i),SIGUSR1);
+
+            }
+
+        }
+    }
+
+    free(date1);
+    free(date2);
+
+
+    char result[512];
+    int bytes;
+    long res = 0;
+    for (size_t i = 0; i < totalWorkers; i++)
+    {
+        do
+        {
+            usleep(100000);
+
+        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
+        res += atol(result);
+        char * tok = NULL;
+        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
+        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
+
+        tok = strtok(currentCountry, delimiters);
+        tok = strtok(NULL, delimiters);
+        tok = strtok(NULL, delimiters);
+        if(!country)
+        {
+            printf("%s %ld\n",tok, atol(result));
+        }
+        else
+        {
+            if(!strcmp(country,tok))
+            {
+                printf("%s %ld\n",tok, atol(result));
+            }
+        }
+
+    }
+
+}
+
+
+
+bool Request_6(char * tok)
+{
+    char message[MAXIMUMBUFFER];
+    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
+
+    char * diseaseID;
+
+    tok = strtok(NULL," ");
+    if(tok == NULL)
+    {
+        printf("error\n");
+        return true;
+    }
+    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+    strcpy(diseaseID,(const  char *)tok);
+
+
+    Date * date1 = NULL;
+    Date * date2 = NULL;
+    date1 = malloc(sizeof(*date1));
+    date2 = malloc(sizeof(*date2));
+
+    // // date1
+    tok = strtok(NULL, delimiters);
+
+    date1 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date1 -> year = (long)atoi(tok);
+
+    // date2
+    tok = strtok(NULL,delimiters);
+
+    if(tok == NULL)
+    {
+        free(date1);
+        free(date2);
+        printf("error\n");
+        return true;
+    }
+    // date2
+    date2 -> day = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> month = (long)atoi(tok);
+
+    tok = strtok(NULL,delimiters);
+    date2 -> year = (long)atoi(tok);
+
+
+    tok = strtok(NULL,delimiters);
+    // without country
+    char * country = NULL;
+
+    if(tok == NULL)
+    {
+
+        for (size_t i = 0; i < totalWorkers; i++)
+        {
+
+            sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
+
+            WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+            kill(GetValue(&workersPidList,i),SIGUSR1);
+        }
+    }
+    else
+    {
+        // store country
+
+        country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
+        strcpy(country,(const  char *)tok);
+
+        for (size_t i = 0; i < totalWorkers; i++)
+        {
+
+            char * path = malloc(sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)) + 1);
+            strcpy(path,GetValue_Path(&subDirectoriesPathList,i));
+            path = strtok(tok,delimiters);
+            path = strtok(tok,delimiters);
+            path = strtok(tok,delimiters);
+
+            if(!strcmp(path,country))
+            {
+                sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld %s",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year, country);
+
+                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+                kill(GetValue(&workersPidList,i),SIGUSR1);
+
+            }
+
+        }
+    }
+
+    free(date1);
+    free(date2);
+
+
+    char result[512];
+    int bytes;
+    long res = 0;
+    for (size_t i = 0; i < totalWorkers; i++)
+    {
+        do
+        {
+            usleep(100000);
+
+        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
+        res += atol(result);
+        char * tok = NULL;
+        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
+        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
+
+        tok = strtok(currentCountry, delimiters);
+        tok = strtok(NULL, delimiters);
+        tok = strtok(NULL, delimiters);
+        if(!country)
+        {
+            printf("%s %ld\n",tok, atol(result));
+        }
+        else
+        {
+            if(!strcmp(country,tok))
+            {
+                printf("%s %ld\n",tok, atol(result));
+            }
+        }
+
+    }
+
+}
+
+
 
 static long Read_Requests_Parse(char * request)
 {
@@ -231,49 +592,36 @@ static long Read_Requests_Parse(char * request)
             return false;
 
         }
-        else if(strcmp(tok,"/diseaseFrequency") == 0)
+        else if(!strcmp(tok,"/diseaseFrequency"))
         {
             Request_2(tok);
-
             return false;
 
         }
-        // else if(strcmp(tok,"/diseaseFrequency") == 0)
-        // {
-        //     Request_2(diseaseHash,tok);
-        //
-        //     return false;
-        //
-        // }
-        // else if(strcmp(tok,"/topk-Diseases") == 0)
-        // {
-        //
-        //     Request_3(countryHash,tok);
-        //
-        // }
-        // else if(strcmp(tok,"/topk-Countries") == 0)
-        // {
-        //     Request_4(diseaseHash,tok);
-        //
-        //     return false;
-        //
-        // }
+        else if(!strcmp(tok,"/topk-AgeRanges"))
+        {
+            Request_3(tok);
+            return false;
+
+        }
         else if(!strcmp(tok,"/searchPatientRecord"))
         {
-
             tok = strtok(NULL, delimiters);
             Request_4(tok);
             return false;
         }
-        // else if(strcmp(tok,"/recordPatientExit") == 0)
-        // {
-        //     if(Request_6(patientHash,tok))
-        //     {
-        //         printf("\nRecord updated\n");
-        //     }
-        //     return false;
-        //
-        // }
+        else if(!strcmp(tok,"/numPatientAdmissions"))
+        {
+            Request_5(tok);
+            return false;
+        }
+        else if(!strcmp(tok,"/numPatientDischarges"))
+        {
+            Request_6(tok);
+            return false;
+        }
+        // /diseaseFrequency COVID-2019 10-10-2010 10-10-2020
+        // /numPatientAdmissions COVID-2019 10-10-2010 10-10-2020
         // else if(strcmp(tok,"/numCurrentPatients") == 0)
         // {
         //     Request_7(diseaseHash,tok);
@@ -293,7 +641,7 @@ static long Read_Requests_Parse(char * request)
 
 }
 
-
+// /topk-AgeRanges 10 Greece COVID-2019 10-10-2010 10-10-2020
 
 long Read_Requests()
 {
