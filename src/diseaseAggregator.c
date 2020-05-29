98897018d0b2786e82939373435991c7da7538dd
ApChris <achris.info@gmail.com>
2020-05-29 14:20:59 +0300

    main has been cleaned version 1

--- src/diseaseAggregator.c
+++ src/diseaseAggregator.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 
-
-#include "../include/parentFunctions.h"
+#include "../include/requests.h"
 
 Node * writeNamedPipeList = NULL;
 long totalWorkers;
@@ -13,667 +12,46 @@ Node * readNamedPipeList = NULL;
 long flagEliminate = 0;
 PathNode * subDirectoriesPathList = NULL;
 
-void Request_1();
-void Request_2(char * tok);
-void Request_3(char * tok);
-void Request_4(char * recordID);
-void Request_5(char * tok);
-void Request_6(char * tok);
+
 
 void StartReadingFiles_Workers()
 {
     printf("\n\n\n\n\n\n\n\nStart Reading\n");
     char message[MAXIMUMBUFFER];
-    printf("%ld\n",LenOfList(subDirectoriesPathList));
+    printf("Check %ld %ld\n",LenOfList(subDirectoriesPathList), bufferSize);
 
     for (long i = 0; i < totalWorkers; i++)
     {
+
         sprintf(message,"/ReadingFiles %ld %s",i,GetValue_Path(&subDirectoriesPathList,i));
+
         WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
+
         kill(GetValue(&workersPidList,i),SIGUSR1);
     }
     sleep(1);
-    char result[512];
-    int bytes;
+
+    char result[MAXIMUMBUFFER] = "";
     for (long i = 0; i < totalWorkers; i++)
     {
+
+        long bytes = 0;
+
         do
         {
-            usleep(100*1000);
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
+            usleep(1);
+        }while((bytes = ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
+
         printf("%s\n",result);
-    }
-
-
-
-}
-
-void Request_1()
-{
-
-    char message[MAXIMUMBUFFER];
-
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        // printf("pid = %ld\n", GetValue(&workersPidList,i));
-        sprintf(message,"/listCountries %s",GetValue_Path(&subDirectoriesPathList,i));
-        WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-        kill(GetValue(&workersPidList,i),SIGUSR1);
-
-    }
-
-    char result[512];
-    int bytes;
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        do
-        {
-            usleep(100000);
-
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-    }
-
-}
-
-void Request_2(char * tok)
-{
-    char message[MAXIMUMBUFFER];
-    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
-
-    char * diseaseID;
-
-    tok = strtok(NULL," ");
-    if(tok == NULL)
-    {
-        printf("error\n");
-        return;
-    }
-    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(diseaseID,(const  char *)tok);
-
-
-    Date * date1 = NULL;
-    Date * date2 = NULL;
-    date1 = malloc(sizeof(*date1));
-    date2 = malloc(sizeof(*date2));
-
-    // // date1
-    tok = strtok(NULL, delimiters);
-
-    date1 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> year = (long)atoi(tok);
-
-    // date2
-    tok = strtok(NULL,delimiters);
-
-    if(tok == NULL)
-    {
-        free(date1);
-        free(date2);
-        printf("error\n");
-        return;
-    }
-    // date2
-    date2 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> year = (long)atoi(tok);
-// diseaseFrequency H1N1 10-10-2010 20-20-2020 Greece
-
-    tok = strtok(NULL,delimiters);
-    // without country
-
-    if(tok == NULL)
-    {
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            sprintf(message,"/diseaseFrequency %s %ld-%ld-%ld %ld-%ld-%ld",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
-
-            WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-            kill(GetValue(&workersPidList,i),SIGUSR1);
-        }
-    }
-    else
-    {
-        // store country
-        char * country;
-        country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-        strcpy(country,(const  char *)tok);
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            char * path = malloc(sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)) + 1);
-            strcpy(path,GetValue_Path(&subDirectoriesPathList,i));
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-
-            if(!strcmp(path,country))
-            {
-                sprintf(message,"/diseaseFrequency %s %ld-%ld-%ld %ld-%ld-%ld %s",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year, country);
-
-                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-                kill(GetValue(&workersPidList,i),SIGUSR1);
-
-            }
-
-        }
-    }
-
-    free(date1);
-    free(date2);
-
-
-    char result[512];
-    int bytes;
-    long res = 0;
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        do
-        {
-            usleep(100000);
-
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-        res += atol(result);
-
-    }
-    printf("%ld\n",res);
-}
-
-void Request_3(char * tok)
-{
-    char message[MAXIMUMBUFFER];
-    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
-
-    // store k
-    char * k;
-    tok = strtok(NULL," ");
-    k = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(k,(const  char *)tok);
-
-    // store country
-    tok = strtok(NULL," ");
-    char * country;
-    country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(country,(const  char *)tok);
-
-    // store diseaseID
-    char * diseaseID;
-    tok = strtok(NULL," ");
-    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(diseaseID,(const  char *)tok);
-
-
-    Date * date1 = NULL;
-    Date * date2 = NULL;
-    date1 = malloc(sizeof(*date1));
-    date2 = malloc(sizeof(*date2));
-
-    // // date1
-    tok = strtok(NULL, delimiters);
-    date1 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> year = (long)atoi(tok);
-
-    // date2
-    tok = strtok(NULL,delimiters);
-    date2 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> year = (long)atoi(tok);
-    //
-    //
-
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        tok = NULL;
-        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
-        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
-
-        tok = strtok(currentCountry, delimiters);
-        tok = strtok(NULL, delimiters);
-        tok = strtok(NULL, delimiters);
-        if(!country)
-        {
-            printf("Flag\n");
-        }
-        else
-        {
-            if(!strcmp(country,tok))
-            {
-                sprintf(message,"/topk-AgeRanges %s %s %s %ld-%ld-%ld %ld-%ld-%ld",k, country, diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
-
-                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-                kill(GetValue(&workersPidList,i),SIGUSR1);
-            }
-        }
-
-    }
-    //
-    //
-    //
-    //
-    // free(date1);
-    // free(date2);
-    //
-
-    char result[MAXBUFFER];
-    int bytes;
-
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
-        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
-
-        tok = strtok(currentCountry, delimiters);
-        tok = strtok(NULL, delimiters);
-        tok = strtok(NULL, delimiters);
-
-            if(!strcmp(country,tok))
-            {
-                do
-                {
-                    usleep(100000);
-
-                }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-
-                printf("%s\n",result);
-            }
-
-
-    }
-
-
-
-    // printf("%ld\n",res);
-}
-
-// /topk-AgeRanges 3 China COVID-2019 10-10-2010 10-10-2020
-
-void Request_4(char * recordID)
-{
-
-    char message[MAXIMUMBUFFER];
-
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        sprintf(message,"/searchPatientRecord %s",recordID);
-        WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-        kill(GetValue(&workersPidList,i),SIGUSR1);
-
-    }
-
-    char result[512];
-    int bytes;
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        do
-        {
-            usleep(100000);
-
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-        if(bytes > 0)
-        {
-            printf("%s\n", result);
-        }
-
-    }
-
-}
-
-
-void Request_5(char * tok)
-{
-    char message[MAXIMUMBUFFER];
-    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
-
-    char * diseaseID;
-
-    tok = strtok(NULL," ");
-    if(tok == NULL)
-    {
-        printf("error\n");
-        return;
-    }
-    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(diseaseID,(const  char *)tok);
-
-
-    Date * date1 = NULL;
-    Date * date2 = NULL;
-    date1 = malloc(sizeof(*date1));
-    date2 = malloc(sizeof(*date2));
-
-    // // date1
-    tok = strtok(NULL, delimiters);
-
-    date1 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> year = (long)atoi(tok);
-
-    // date2
-    tok = strtok(NULL,delimiters);
-
-    if(tok == NULL)
-    {
-        free(date1);
-        free(date2);
-        printf("error\n");
-        return;
-    }
-    // date2
-    date2 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> year = (long)atoi(tok);
-
-
-    tok = strtok(NULL,delimiters);
-    // without country
-    char * country = NULL;
-
-    if(tok == NULL)
-    {
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
-
-            WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-            kill(GetValue(&workersPidList,i),SIGUSR1);
-        }
-    }
-    else
-    {
-        // store country
-
-        country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-        strcpy(country,(const  char *)tok);
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            char * path = malloc(sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)) + 1);
-            strcpy(path,GetValue_Path(&subDirectoriesPathList,i));
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-
-            if(!strcmp(path,country))
-            {
-                sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld %s",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year, country);
-
-                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-                kill(GetValue(&workersPidList,i),SIGUSR1);
-
-            }
-
-        }
-    }
-
-    free(date1);
-    free(date2);
-
-
-    char result[512];
-    int bytes;
-    long res = 0;
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        do
-        {
-            usleep(100000);
-
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-        res += atol(result);
-        char * tok = NULL;
-        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
-        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
-
-        tok = strtok(currentCountry, delimiters);
-        tok = strtok(NULL, delimiters);
-        tok = strtok(NULL, delimiters);
-        if(!country)
-        {
-            printf("%s %ld\n",tok, atol(result));
-        }
-        else
-        {
-            if(!strcmp(country,tok))
-            {
-                printf("%s %ld\n",tok, atol(result));
-            }
-        }
-
+        printf("--------------------------------------\n");
     }
 
 }
 
 
 
-void Request_6(char * tok)
-{
-    char message[MAXIMUMBUFFER];
-    char delimiters[] = " \n\t\r\v\f\n-:,/.><[]{}|-=+*@#$;";
 
-    char * diseaseID;
-
-    tok = strtok(NULL," ");
-    if(tok == NULL)
-    {
-        printf("error\n");
-        return;
-    }
-    diseaseID = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-    strcpy(diseaseID,(const  char *)tok);
-
-
-    Date * date1 = NULL;
-    Date * date2 = NULL;
-    date1 = malloc(sizeof(*date1));
-    date2 = malloc(sizeof(*date2));
-
-    // // date1
-    tok = strtok(NULL, delimiters);
-
-    date1 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date1 -> year = (long)atoi(tok);
-
-    // date2
-    tok = strtok(NULL,delimiters);
-
-    if(tok == NULL)
-    {
-        free(date1);
-        free(date2);
-        printf("error\n");
-        return;
-    }
-    // date2
-    date2 -> day = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> month = (long)atoi(tok);
-
-    tok = strtok(NULL,delimiters);
-    date2 -> year = (long)atoi(tok);
-
-
-    tok = strtok(NULL,delimiters);
-    // without country
-    char * country = NULL;
-
-    if(tok == NULL)
-    {
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year);
-
-            WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-            kill(GetValue(&workersPidList,i),SIGUSR1);
-        }
-    }
-    else
-    {
-        // store country
-
-        country = ( char *)malloc(1 + sizeof(char) * strlen(tok));
-        strcpy(country,(const  char *)tok);
-
-        for (long i = 0; i < totalWorkers; i++)
-        {
-
-            char * path = malloc(sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)) + 1);
-            strcpy(path,GetValue_Path(&subDirectoriesPathList,i));
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-            path = strtok(tok,delimiters);
-
-            if(!strcmp(path,country))
-            {
-                sprintf(message,"/numPatientAdmissions %s %ld-%ld-%ld %ld-%ld-%ld %s",diseaseID, date1 -> day, date1 -> month, date1 -> year,date2 -> day, date2 -> month, date2 -> year, country);
-
-                WriteToNamedPipe(GetValue(&writeNamedPipeList,i), message);
-                kill(GetValue(&workersPidList,i),SIGUSR1);
-
-            }
-
-        }
-    }
-
-    free(date1);
-    free(date2);
-
-
-    char result[512];
-    int bytes;
-    long res = 0;
-    for (long i = 0; i < totalWorkers; i++)
-    {
-        do
-        {
-            usleep(100000);
-
-        }while((bytes=ReadFromNamedPipe(GetValue(&readNamedPipeList,i), result))<=0);
-        res += atol(result);
-        char * tok = NULL;
-        char * currentCountry = (char *)malloc(1 + sizeof(char)*strlen(GetValue_Path(&subDirectoriesPathList,i)));
-        strcpy(currentCountry, GetValue_Path(&subDirectoriesPathList,i));
-
-        tok = strtok(currentCountry, delimiters);
-        tok = strtok(NULL, delimiters);
-        tok = strtok(NULL, delimiters);
-        if(!country)
-        {
-            printf("%s %ld\n",tok, atol(result));
-        }
-        else
-        {
-            if(!strcmp(country,tok))
-            {
-                printf("%s %ld\n",tok, atol(result));
-            }
-        }
-
-    }
-
-}
-
-
-
-static long Read_Requests_Parse(char * request)
-{
-
-    char * tok;
-    char delimiters[] = " \n\t\r\v\f\n:,.><[]{}|=+*@#$";
-    tok = strtok(request,delimiters);
-
-    if(tok != NULL)
-    {
-        if(!strcmp(tok,"/listCountries"))
-        {
-            Request_1();
-
-            return false;
-
-        }
-        else if(!strcmp(tok,"/diseaseFrequency"))
-        {
-            Request_2(tok);
-            return false;
-
-        }
-        else if(!strcmp(tok,"/topk-AgeRanges"))
-        {
-            Request_3(tok);
-            return false;
-
-        }
-        else if(!strcmp(tok,"/searchPatientRecord"))
-        {
-            tok = strtok(NULL, delimiters);
-            Request_4(tok);
-            return false;
-        }
-        else if(!strcmp(tok,"/numPatientAdmissions"))
-        {
-            Request_5(tok);
-            return false;
-        }
-        else if(!strcmp(tok,"/numPatientDischarges"))
-        {
-            Request_6(tok);
-            return false;
-        }
-        // /diseaseFrequency COVID-2019 10-10-2010 10-10-2020
-
-
-        else if(!strcmp(tok,"/exit"))
-        {
-            printf("exiting\n");
-            Elimination();
-            return true;
-
-        }
-        return false;
-
-    }
-    return true;
-
-}
+// ./create_infiles.sh ../../etc/diseaseFile.txt ../../etc/countriesFile.txt input_dir 5 400
 
 
 // /listCountries
@@ -687,21 +65,7 @@ static long Read_Requests_Parse(char * request)
 // /topk-AgeRanges 4 China COVID-2019 10-10-2010 10-10-2020
 // /topk-AgeRanges 3 USA COVID-2019 10-10-2010 10-10-2020
 // /topk-AgeRanges 4 China MERS-COV 10-10-2010 10-10-2020
-long Read_Requests()
-{
-    char * request = NULL;
-    size_t length;
-    long read;
-    while((read = getline(&request,&length, stdin)) != -1)
-    {
-        if(Read_Requests_Parse(request))
-        {
-            free(request);
-            break;
-        }
-    }
-    return -1;
-}
+
 
 
 int main(int argc, char const *argv[])
@@ -714,10 +78,10 @@ int main(int argc, char const *argv[])
     sigaction(SIGTERM, &action, NULL);
     // sigaction(SIGKILL, &action, NULL);
 
-    static struct sigaction workerAction;
-    workerAction.sa_handler = ReCreateWorker;
-    sigfillset(&(workerAction.sa_mask));
-    sigaction(SIGCHLD, &workerAction, NULL);
+    // static struct sigaction workerAction;
+    // workerAction.sa_handler = ReCreateWorker;
+    // sigfillset(&(workerAction.sa_mask));
+    // sigaction(SIGCHLD, &workerAction, NULL);
 
     if (argc != 7)                          // Check if we have !=7 arguments
     {
@@ -809,7 +173,6 @@ int main(int argc, char const *argv[])
         PushNode(&readNamedPipeList,fd);
     }
 
-    // ./create_infiles.sh ../../etc/diseaseFile.txt ../../etc/countriesFile.txt input_dir 5 400
 
     printf("Waiting!!!\n");
     sleep(1);
